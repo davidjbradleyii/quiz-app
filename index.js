5ac5057570e8179f88d246db475cb3920831e9a3
David B <d.jbradley.ii@gmail.com>
2019-07-11 13:48:36 -0700

    adds quiz object with questions, choices and answerIndex

---
+++ index.js
@@ -0,0 +1,54 @@
+const quiz = {
+    name: 'Friends',
+    questions:[
+    {
+        question:"How many babies did Phoebe carry for her brother?",
+        choices:[1, 3, 0, 2],
+        answerIndex: 1
+    },
+    {
+        question:"Where did Phoebe and Mike get married?",
+        choices:["On the street", "In a church", "In a park", "They never got married"],
+        answerIndex: 0
+    },
+    {
+        question:"The 'Geller Cup' is a prize in which sport?",
+        choices:["Hockey", "Basketball", "Football", "Rugby"],
+        answerIndex: 2 
+    },
+    {
+        question:"Whose catchphrase is 'Oh my GOD'?",
+        choices:["Phoebe","Ross","Janice","Chandler"],
+        answerIndex: 2
+    },
+    {
+        question:"How many friends have worked in the coffee shop?",
+        choices:[1, 5, 3, 2],
+        answerIndex: 3
+    },
+    {
+        question:"What type of animal is 'Hugsy'?",
+        choices:["Lion","Penguin","Rabbit","Bear"],
+        answerIndex: 1
+    },
+    {
+        question:"Who was the holiday Armadillo?",
+        choices:["Joey","Mike","Chandler","Ross"],
+        answerIndex: 3
+    },
+    {
+        question:"Who plays 'Will', an old friend of Monica and Ross from High School?",
+        choices:["Bruce Willis","Paul Rudd","Brad Pitt","Michael Rapaport"],
+        answerIndex: 2
+    },
+    {
+        question:"What does Monica's dad give her to compensate for ruining her childhood possessions?",
+        choices:["Porsche","Money","House","Nothing"],
+        answerIndex: 0
+    },
+    {
+        question:"Where is Chandler forced to work after falling asleep in a meeting?",
+        choices:["Yemen","Atlantic City","Las Vegas","Tulsa"],
+        answerIndex: 3
+    }]
+};
